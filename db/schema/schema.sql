edb36af52cf2f24e9b9b35d69cbad63d4078969a
Le Nguyen Hai Dang <danglnh07.work@gmail.com>
2025-09-08 15:25:56 +0700

    Update URL shortener logic
    - db/schema/schema.sql: remove the domain and shorten_id fields
    - service/url_shortener.go: use base62 to encode URL using the ID in
      database as encoding source

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -1,9 +1,7 @@
 -- Create table url
 CREATE TABLE IF NOT EXISTS url (
     id SERIAL PRIMARY KEY,
-    domain VARCHAR(20) NOT NULL, -- Domain of the URL, like www.facebook.com, which won't change
-    original_url VARCHAR(200) NOT NULL, -- Original full URL 
-    shorten_id VARCHAR(64) NOT NULL, -- The generated ID used in the shorten URL.
+    original_url VARCHAR NOT NULL UNIQUE, -- Original full URL, no max size
     time_created TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
--- service/url_shortener.go
+++ service/url_shortener.go
@@ -1,7 +1,44 @@
 package service
 
-import "encoding/base64"
+const (
+	base62chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	base        = 62
+)
 
-func GenerateID(url string) string {
-	return base64.URLEncoding.EncodeToString([]byte(url))
+// Encode a number into Base62
+func EncodeBase62(num int64) string {
+	/*
+	 * Algorithm explains: just like with other base conversion, we repeatedly divide the number
+	 * to 62 and append the characters at [remainder] position in the characters set
+	 */
+
+	if num == 0 {
+		return string(base62chars[0])
+	}
+
+	result := ""
+	for num > 0 {
+		remainder := num % base
+		result = string(base62chars[remainder]) + result
+		num = num / base
+	}
+	return result
+}
+
+// Decode Base62 string back to number
+func DecodeBase62(str string) int64 {
+	var num int64
+	for _, c := range str {
+		num *= base
+		switch {
+		case '0' <= c && c <= '9':
+			num += int64(c - '0')
+		case 'A' <= c && c <= 'Z':
+			num += int64(c-'A') + 10
+		case 'a' <= c && c <= 'z':
+			num += int64(c-'a') + 36
+		}
+	}
+
+	return num
 }
